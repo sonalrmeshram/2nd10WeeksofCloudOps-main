b896c089d857a2d565484acade442f0055fbaf76
sonalrmeshram <sonalrmeshram@gmail.com>
2026-01-30 11:35:35 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://20.255.54.10";
+const API_BASE_URL = "http://ab7a1ac6492ca4bcf81cf9c0dd6e7125-1152399334.us-east-1.elb.amazonaws.com";
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
